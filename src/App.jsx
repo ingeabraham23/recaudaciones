8b0fa723462a840bb61bcfadb5d62875725ae4d0
Abraham Cabrera <ingeabraham23@gmail.com>
2024-04-23 17:58:02 -0600

    primer commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+// eslint-disable-next-line no-unused-vars
+import React from 'react'
+import Recaudaciones from './components/Recaudaciones'
+
+function App() {
+
+  return (
+    <>
+    <Recaudaciones></Recaudaciones>
+    </>
+  )
+}
+
+export default App
---
+++ src/components/Recaudaciones.jsx
@@ -0,0 +1,208 @@
+// eslint-disable-next-line no-unused-vars
+import React from "react";
+import "./Recaudaciones.css";
+
+const Recaudaciones = () => {
+  return (
+    <div className="contenedor">
+      <h2>
+        Política de Cooperación entre Compañeros en Caso de Hospitalización o
+        Fallecimiento de Familiares.
+      </h2>
+      <p>Estimados compañeros,</p>
+      <p>
+        En nuestra comunidad laboral, valoramos la empatía y el apoyo mutuo en
+        los momentos de dificultad personal. Sabemos que la vida puede presentar
+        desafíos inesperados, y como compañeros, deseamos estar ahí para
+        apoyarnos mutuamente en caso de situaciones de emergencia relacionadas
+        con la salud de nuestros seres queridos.
+      </p>
+      <p>
+        Por ello, hemos establecido una política de cooperación entre compañeros
+        para brindar apoyo financiero a aquellos que se vean afectados por
+        hospitalizaciones o fallecimientos de sus familiares más cercanos. Esta
+        política se aplica a los siguientes familiares:
+      </p>
+      <p>
+        <ul>
+          <li>
+            <strong>Hijos</strong>: Incluyendo hijos biológicos, adoptivos y los
+            hijos de la pareja.{" "}
+          </li>
+          <li>
+            <strong>Hermanos</strong>: Hermanos y hermanas biológicos y
+            adoptivos.
+          </li>
+          <li>
+            <strong>Esposa</strong>: Cónyuge legal o pareja de hecho reconocida.{" "}
+          </li>
+          <li>
+            <strong>Padres</strong>: Padres biológicos y adoptivos.{" "}
+          </li>
+          <li>
+            <strong>Abuelos</strong>: Abuelos y abuelas.{" "}
+          </li>
+          <li>
+            <strong>Suegros</strong>: Padres del cónyuge o pareja de hecho.
+          </li>
+        </ul>
+      </p>
+      <p>
+        En el caso de que uno de nuestros compañeros se enfrente a una situación
+        de hospitalización o fallecimiento de alguno de estos familiares, les
+        alentamos a ponerse en contacto con Abraham Cabrera Benito al número 231
+        159 1893, quien se encargará de comunicar la situación a todos los
+        compañeros a través de las redes sociales.
+      </p>
+      <p>
+        Posteriormente, el compañero afectado coordinará la recolección de
+        fondos. En caso de que no pueda hacerlo personalmente, deberá designar a
+        un familiar o a un amigo entre los compañeros para llevar a cabo esta
+        tarea. Alternativamente, otro compañero puede ofrecerse voluntariamente
+        para encargarse de la recolección.
+      </p>
+      <p>
+        El apoyo financiero se proporcionará en forma de donaciones voluntarias
+        de los compañeros interesados. Cada aportación será completamente
+        opcional.
+      </p>
+      <p>
+        Para garantizar la transparencia en el proceso, se requerirá que se
+        publique en las redes sociales una lista detallada de cada colaborador y
+        el total recaudado. Para ello, se deberá comunicar con Abraham Cabrera
+        Benito, quien estará a cargo de la logística y las comunicaciones.
+      </p>
+      <p>
+        Además, se recomienda encarecidamente que al entregar el efectivo, se
+        verifique que la cantidad coincida con la registrada en la lista. Esta
+        verificación deberá ser confirmada tanto por la persona que entrega como
+        por la que recibe.
+      </p>
+      <p>
+        Agradecemos su compromiso con los valores de solidaridad y apoyo en
+        nuestro grupo. Juntos, podemos afrontar las dificultades de la vida y
+        asegurarnos de que todos nuestros compañeros se sientan respaldados en
+        momentos de necesidad.
+      </p>
+      <div className="separador"></div>
+      <h2>Cronología de Recaudaciones.</h2>
+      <h1>2021</h1>
+      <div className="separador"></div>
+      <p>
+        01. Recaudación para apoyar al compañero Ivan. Checador de INE.
+        Fallecimiento de su Abuelita.jueves, 25 de noviembre de 2021.
+      </p>
+      <img style={{ width: "95%", height: "auto" }} src="./01.png" />
+      <h1>2022</h1>
+      <div className="separador"></div>
+      <p>
+        02. Recaudación para apoyar al compañero Candido. Operador de la unidad
+        12. Fallecimiento de su señora madre.viernes, 15 de julio de 2022.
+      </p>
+      <img style={{ width: "95%", height: "auto" }} src="./02.png" />
+      <div className="separador"></div>
+      <p>
+        03. Recaudación para apoyar al compañero Aldo. Posturero de la unidad
+        43, 38, 18. Fallecimiento de su Padre. Pony unidad 46 U-R.miércoles, 7
+        de diciembre de 2022.
+      </p>
+      <img style={{ width: "95%", height: "auto" }} src="./03.png" />
+      <div className="separador"></div>
+      <h1>2023</h1>
+      <p>
+        04. Recaudación para apoyar al compañero Fermin. Operador de la unidad
+        04. Su hijo se encuentra internado en el hospital.viernes, 20 de enero
+        de 2023.
+      </p>
+      <img style={{ width: "95%", height: "auto" }} src="./04.png" />
+      <div className="separador"></div>
+      <p>
+        05. Recaudación para apoyar al compañero Ramon. Operador de la unidad
+        03. viernes, 24 de febrero de 2023.
+      </p>
+      <img style={{ width: "95%", height: "auto" }} src="./05.png" />
+      <div className="separador"></div>
+      <p>
+        06. Recaudación para apoyar al compañero Eloy. Operador de la unidad 25.
+        Operación de su señora esposa. martes, 30 de mayo de 2023.
+      </p>
+      <img style={{ width: "95%", height: "auto" }} src="./06.png" />
+      <div className="separador"></div>
+      <p>
+        07. Recaudación para apoyar al compañero Jesus. Operador de la unidad
+        46. Fallecimiento de su señora esposa. domingo, 18 de junio de 2023.
+      </p>
+      <img style={{ width: "95%", height: "auto" }} src="./07.png" />
+      <div className="separador"></div>
+      <p>
+        08. Recaudación para apoyar al compañero Ramiro. Posturero de la unidad
+        5, 8, 13, 33. Hospitalizacion de su madre. domingo, 2 de julio de 2023.
+      </p>
+      <img style={{ width: "95%", height: "auto" }} src="./08.png" />
+      <div className="separador"></div>
+      <p>
+        09. Recaudación para apoyar al compañero Angel. Operador de la unidad
+        43. Se encuentra internado en el hospital.jueves, 17 de agosto de 2023.
+      </p>
+      <img style={{ width: "95%", height: "auto" }} src="./09.png" />
+      <div className="separador"></div>
+      <p>
+        10. Recaudación para apoyar al compañero Zamora Posturero de la unidad
+        27, 25, 20. Fallecimiento de su Padre.domingo, 15 de octubre de 2023.
+      </p>
+      <img style={{ width: "95%", height: "auto" }} src="./10.png" />
+      <div className="separador"></div>
+      <p>
+        11. Recaudación para apoyar al compañero Pedro. Operador de la unidad
+        01. Se encuentra hospitalizado en puebla por una bactería en los
+        pulmones. lunes, 13 de noviembre de 2023.
+      </p>
+      <img style={{ width: "95%", height: "auto" }} src="./11.png" />
+      <div className="separador"></div>
+      <p>
+        12. Recaudación para apoyar al compañero Aldo. Posturero de la unidad
+        43, 38, 18. Fallecimiento de su abuelito. domingo, 17 de diciembre de
+        2023.
+      </p>
+      <img style={{ width: "95%", height: "auto" }} src="./12.png" />
+      <div className="separador"></div>
+      <h1>2024</h1>
+      <div className="separador"></div>
+      <p>
+        13. Recaudación para apoyar al compañero. Pedro, Operador de la unidad
+        01. Se encuentra enfermo. Recauda Heriberto, Eloy y Abraham. jueves, 04
+        de enero de 2024.
+      </p>
+      <img style={{ width: "95%", height: "auto" }} src="./13.png" />
+      <div className="separador"></div>
+      <p>
+        14. Recaudación para apoyar a la familia del compañero. Pedro, quien en
+        vida fue operador de la unidad 01. Fallecimiento. Recauda Heriberto,
+        Eloy y Abraham. martes, 26 de marzo de 2024.
+      </p>
+      <img style={{ width: "95%", height: "auto" }} src="./14.png" />
+      <div className="separador"></div>
+      <p>
+        15. Recaudación para apoyar al compañero Juan Fernandez. Lavador. Su
+        madre se encuentra enferma.Recauda Oscar. sabado, 06 de abril de 2024.
+      </p>
+      <img style={{ width: "95%", height: "auto" }} src="./15.png" />
+      <div className="separador"></div>
+      <p>
+        16. Recaudación para apoyar al compañero Heriberto. Operador de la
+        unidad 16. Estuvo internado en el hospital y se le realizo una cirugía.
+        Recauda Genaro Gato padilla. jueves, 11 de abril de 2024.
+      </p>
+      <img style={{ width: "95%", height: "auto" }} src="./16.png" />
+      <div className="separador"></div>
+      <p>
+        17. Recaudación para apoyar al compañero_ David , Operador de la unidad_
+        18, (fallecimiento de su suegra). Recauda_ Abraham, Eloy y Heriberto.
+        sábado, 20 de abril de 2024.
+      </p>
+      <img style={{ width: "95%", height: "auto" }} src="./17.png" />
+    </div>
+  );
+};
+
+export default Recaudaciones;
